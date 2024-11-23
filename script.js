55198f1fb7f678182c89218815ecff6be7bde3a6
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-23 22:56:02 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,15 @@
+for (var x = 1; x<=100; x=x+10) {
+    for(var y=x; y<x+10; y++){
+        document.write(y+" ");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+
+for (var x = 1; x<=5; x++) {
+    for(var y=1; y<=x; y++){
+        document.write(y+" ");
+    }
+    document.write("<br>");
+}
